b2f8bf3870d0bc8002329ba3d1d7541d36aed02f
sthompson4 <32102091+sthompson4@users.noreply.github.com>
2017-09-29 11:58:28 +0100

    Add files via upload

---
+++ Project/Project/MainScreen.Designer.vb
@@ -0,0 +1,68 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Game
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer = New System.Windows.Forms.Timer(Me.components)
+        Me.Board = New System.Windows.Forms.PictureBox()
+        Me.Start = New System.Windows.Forms.Button()
+        CType(Me.Board, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Board
+        '
+        Me.Board.Image = Global.Project.My.Resources.Resources.ChessBoard
+        Me.Board.InitialImage = Global.Project.My.Resources.Resources.ChessBoard
+        Me.Board.Location = New System.Drawing.Point(217, 44)
+        Me.Board.Name = "Board"
+        Me.Board.Size = New System.Drawing.Size(803, 806)
+        Me.Board.TabIndex = 0
+        Me.Board.TabStop = False
+        '
+        'Start
+        '
+        Me.Start.Location = New System.Drawing.Point(28, 44)
+        Me.Start.Name = "Start"
+        Me.Start.Size = New System.Drawing.Size(75, 23)
+        Me.Start.TabIndex = 1
+        Me.Start.Text = "Start"
+        Me.Start.UseVisualStyleBackColor = True
+        '
+        'Game
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1241, 898)
+        Me.Controls.Add(Me.Start)
+        Me.Controls.Add(Me.Board)
+        Me.Name = "Game"
+        Me.Text = "Screen"
+        CType(Me.Board, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Timer As Timer
+    Public WithEvents Board As PictureBox
+    Friend WithEvents Start As Button
+End Class
---
+++ Project/Project/MainScreen.vb
@@ -0,0 +1,65 @@
+﻿Public Class Game
+
+    Sub Setup()
+        Dim Piece As String
+        Dim X As Integer
+        Dim Y As Integer
+        Dim Board As New CBoard
+        Dim e As PaintEventArgs
+
+        Y = 1
+        For X = 0 To 7
+            Piece = "WPawn"
+            Board.DrawPiece(e, X, Y, Piece)
+        Next
+        Y = 6
+        For X = 0 To 7
+            Piece = "BPawn"
+            Board.DrawPiece(e, X, Y, Piece)
+        Next
+
+        Y = 0
+        X = 0
+        Board.DrawPiece(e, X, Y, "WCastle")
+        X = 7
+        Board.DrawPiece(e, X, Y, "WCastle")
+
+        X = 1
+        Board.DrawPiece(e, X, Y, "WCastle")
+
+    End Sub
+
+    Private Sub Board_MouseClick(sender As Object, e As MouseEventArgs) Handles Board.MouseClick
+        Dim X As Integer
+        Dim Y As Integer
+
+        X = e.Location.X
+        Y = e.Location.Y
+
+        X = X \ 100
+        Y = Y \ 100
+
+    End Sub
+
+    Private Sub Start_Click(sender As Object, e As EventArgs) Handles Start.Click
+        MessageBox.Show(Start, "Start Game?")
+        Setup()
+
+    End Sub
+End Class
+
+Public Class CBoard
+    Dim Square(7, 7) As String
+    Public Sub DrawPiece(ByVal e As System.Windows.Forms.PaintEventArgs, X As Integer, Y As Integer, Piece As String)
+        Dim PieceImage As Image
+        PieceImage = Image.FromFile("U:\Visual Studio 2017\Projects\Project\Project\bin\Release\ChessAssets\" & Piece & ".png")
+        Square(X, Y) = Piece
+        e.Graphics.DrawImage(PieceImage, ((X + 1) * 100), ((Y + 1) * 100), 100, 100)
+
+    End Sub
+    Public Sub SelectPiece(X, Y)
+        Dim Piece As String
+        Piece = Square(1, 1)
+    End Sub
+End Class
+
